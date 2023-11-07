7a36bcac4b0fffb0a6f4438fb2cfda2d739f2e10
Panth <panth977@Panths-MacBook-Pro.local>
2023-11-07 22:37:56 +0530

    exporting formsnapshot parser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import DB from './db';
-import { MetaParser } from './parser';
+import { MetaParser, fromSnapshotToJson } from './parser';
 
 export { getDocRef, getQueryRef, getQueryParams } from './ref';
 export { DB, MetaParser };
+export { fromSnapshotToJson as parseFromSnapshot };
 export * from './apis';
